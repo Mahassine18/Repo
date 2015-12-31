4ad764df739ccad357febb44a473bcc086688d82
abdessamad mahassine <abdessamad 7mahassine@gmail.com>
2015-12-31 09:46:00 +0000

    ajouter variable S

--- Git test/src/file1.java
+++ Git test/src/file1.java
@@ -3,7 +3,9 @@ public class file1 {
 	public static void main (String arg[]){
 		System.out.println("Bonjour tout le monde");
 		int a=2,b=3;
-		System.out.println(a+b);
+		int S;
+		S=a+b;
+		System.out.println(S);
 	
 	}
 }
