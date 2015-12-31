edc72e3769ddfd768a43057004472e9889caf2e7
abdessamad mahassine <abdessamad 7mahassine@gmail.com>
2015-12-31 09:48:16 +0000

    cree une fonction add

--- Git test/src/file1.java
+++ Git test/src/file1.java
@@ -1,9 +1,13 @@
 
 public class file1 {
+	public static void add (int a, int b){
+		int S;
+		S=a+b;
+		System.out.println(S);
+	}
 	public static void main (String arg[]){
 		System.out.println("Bonjour tout le monde");
-		int a=2,b=3;
-		System.out.println(a+b);
+		
 	
 	}
 }
